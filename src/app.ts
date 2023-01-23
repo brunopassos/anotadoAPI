a2711383e3f60c5e0cfb4be63166b74e43331541
Bruno Passos <brunopassos_bp@hotmail.com>
2023-01-23 13:12:34 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express()
+
+app.use(express.json())
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
