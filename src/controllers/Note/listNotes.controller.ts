1a6e3f39b293422b14af688e0983a1addff928e9
Bruno Passos <brunopassos_bp@hotmail.com>
2023-04-06 11:18:35 -0300

    feat: updated the controllers return

--- src/controllers/Note/listNotes.controller.ts
+++ src/controllers/Note/listNotes.controller.ts
@@ -6,10 +6,7 @@ import listNotesService from "../../services/Note/listNotes.service";
 const listNotesController = async (req: Request, res: Response) => {
     try {
         const notes = await listNotesService();
-        return res.status(200).json(instanceToPlain({data: {
-            message: "Success",
-            notes
-        }}));
+        return res.status(200).json(instanceToPlain(notes));
     } catch (error) {
         if (error instanceof AppError) {
             handleError(error, res)
